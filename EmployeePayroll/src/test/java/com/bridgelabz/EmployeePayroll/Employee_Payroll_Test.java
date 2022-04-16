4203af00da9b2d09ceaa14b5c2c3007a754ae1f0
nehachougule1998 <nchougule734@gmail.com>
2022-04-16 14:44:04 +0530

    Added

---
+++ EmployeePayroll/src/test/java/com/bridgelabz/EmployeePayroll/Employee_Payroll_Test.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.EmployeePayroll;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class Employee_Payroll_Test {
+	
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+}
